61c81aaa50c3639c9049e43d66e49e425ced275c
Ryan Smith <rhydesmith@gmail.com>
2019-10-21 19:36:24 -0400

    fixed the directory detection in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,26 @@
-if [ -d "bio_pyvol.egg-info"]; then
+if [ -d "bio_pyvol.egg-info" ]; then
   rm -r "bio_pyvol.egg-info"
 fi
 
-if [ -d "build"]; then
+if [ -d "build" ]; then
   rm -r "build"
 fi
 
-if [ -d "dist"]; then
+if [ -d "dist" ]; then
   rm -r "dist"
 fi
 
 python setup.py sdist bdist_wheel --universal
 twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
 
-if [ -d "bio_pyvol.egg-info"]; then
+if [ -d "bio_pyvol.egg-info" ]; then
   rm -r "bio_pyvol.egg-info"
 fi
 
-if [ -d "build"]; then
+if [ -d "build" ]; then
   rm -r "build"
 fi
 
-if [ -d "dist"]; then
+if [ -d "dist" ]; then
   rm -r "dist"
 fi
