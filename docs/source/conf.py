f5b3a76740bd15e719ba964c47be7c640ab5bc4c
Ryan Smith <rhydesmith@gmail.com>
2019-10-28 18:54:27 -0400

    sphinx conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,8 +31,17 @@ import shlex
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx_rtd_theme"
+    "sphinx_rtd_theme",
+    "sphinx.ext.napolean",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx"
 ]
+
+napolean_google_docstring = True
+autodoc_member_order = 'bysource'
+autodoc_default_flags = ['members', 'show-inheritance']
+autosummary_generate = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
