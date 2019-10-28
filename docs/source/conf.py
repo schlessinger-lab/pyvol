f3e264b579eb116142b0b2158cdb90cccf230ac6
Ryan Smith <rhydesmith@gmail.com>
2019-10-28 19:43:29 -0400

    rebuilt pdf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ import shlex
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- General configuration ------------------------------------------------
 
@@ -33,7 +33,8 @@ sys.path.insert(0, os.path.abspath('../../'))
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
-    "sphinx.ext.intersphinx"
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon"
 ]
 
 autodoc_member_order = 'bysource'
