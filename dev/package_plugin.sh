1501f9ec17e5cdfd7f29bb0e52a3ecdfe698f2ff
Ryan Smith <rhydesmith@gmail.com>
2019-11-07 15:00:53 -0500

    pushed all changes to github and PyPI; generated new installers

--- dev/package_plugin.sh
+++ dev/package_plugin.sh
@@ -18,7 +18,7 @@ mv ${small_zip_name} ../installers/
 cp ../installers/${small_zip_name} ../installers/pyvol-installer.zip
 
 cd $cache_dir
-# pip download bio-pyvol --no-binary :all:
+pip download bio-pyvol --no-binary :all:
 
 cd ${project_dir}
 zip -r ${full_zip_name} pyvol_gui/
