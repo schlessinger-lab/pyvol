8398aafa64ba031854da46d62b8075305a4a601e
Ryan Smith <rhydesmith@gmail.com>
2019-11-15 19:05:08 -0500

    [auto] rebuilt docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.2.42',
+    VERSION: '1.2.43',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
